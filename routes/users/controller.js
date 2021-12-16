a058c12621608f21606403c7782fb79000979059
WhiteRose <doctorwrose@gmail.com>
2021-12-17 01:28:47 +0700

    fix

--- routes/users/controller.js
+++ routes/users/controller.js
@@ -4,18 +4,26 @@ const prisma = require('../../database/client');
 const usersController = {
 
 	async readUsers(req, res, next){
+		var students = []
 		let result = null;
 		try {
 			result = await prisma.user.findMany({
 				include: {
-					school: true
+					school: true,
+					Idea: true
 				}
 			});
+			result.map(student => {
+				students.push({
+					...student,
+					isCompleted : student.Idea.filter((item) => item.idea?.[0]?.round_1 === "accepted").length === 5 ? true : false
+				})
+			})
 		} catch (e) {
 			next(e);
 			return null;
 		}
-		res.status(200).json(result);
+		res.status(200).json({ students });
 	},
 };
 
